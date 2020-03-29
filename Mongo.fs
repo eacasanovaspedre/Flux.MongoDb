218ffdde49ce6b1561a69284cb7447e97049e6a7
Enrique Alfonso Casanovas Pedre <eacasanovaspedre@outlook.com>
2020-03-29 18:19:07 -0300

    Basic stuff added

---
+++ Mongo.fs
@@ -0,0 +1,142 @@
+﻿namespace Flux.MongoDb
+
+open MongoDB.Driver
+open System.Linq
+open MongoDB.Bson
+open Flux.LanguageUtils.Linq.Expressions
+open Flux
+
+[<Struct>]
+type ConnectionString = ConnectionString of NEString
+
+[<Struct>]
+type Client = Client of MongoClient
+
+[<Struct>]
+type Session =
+    | Session of IClientSessionHandle
+    interface System.IDisposable with
+        member this.Dispose(): unit =
+            match this with
+            | Session session -> session.Dispose()
+
+
+[<Struct>]
+type Database = Database of MongoDB.Driver.IMongoDatabase
+
+[<Struct>]
+type 'T Collection = Collection of 'T MongoDB.Driver.IMongoCollection
+
+[<Struct>]
+type CollectionName = CollectionName of NEString
+
+module ObjectId =
+
+    let inline create() = ObjectId.GenerateNewId()
+
+    let inline asString (objectId: ObjectId) = objectId.ToString()
+
+module ConnectionString =
+
+    let inline create str = ConnectionString str
+
+    let inline rawValue (ConnectionString str) = str
+
+module Client =
+
+    let inline create connectionString =
+        connectionString
+        |> ConnectionString.rawValue
+        |> NEString.stringValue
+        |> MongoClient
+        |> Client
+
+    let inline mongoClient (Client client) = client
+
+    let inline internal getMongoDatabase client = (client |> mongoClient).GetDatabase
+
+module Session =
+
+    let inline start client = (Client.mongoClient client).StartSession() |> Session
+
+    let inline getMongoSession (Session session) = session
+
+module Transaction =
+
+    let inline start (Session session) = session.StartTransaction()
+
+    let inline abort (Session session) = session.AbortTransaction()
+
+    let inline commit (Session session) = session.CommitTransaction()
+
+module Database =
+
+    let inline get dbName client =
+        dbName
+        |> NEString.stringValue
+        |> Client.getMongoDatabase client
+        |> Database
+
+    let inline internal getMongoDatabase (Database db) = db
+
+    let inline internal getMongoCollection database = (getMongoDatabase database).GetCollection<_>
+
+module Collection =
+
+    let inline get collectionName database =
+        collectionName
+        |> NEString.stringValue
+        |> Database.getMongoCollection database
+        |> Collection
+
+    let inline query (Collection collection) = collection.AsQueryable()
+
+    let createDocument createWithId =
+        ()
+        |> ObjectId.create
+        |> ObjectId.asString
+        |> createWithId
+
+    let insertOne (Collection collection) session doc =
+        match session with
+        | None -> collection.InsertOne doc
+        | Some(Session session) -> collection.InsertOne(session, doc)
+
+    let insertMany (Collection collection) session docs =
+        match session with
+        | None -> collection.InsertMany(docs)
+        | Some(Session session) -> collection.InsertMany(session, docs)
+
+    let insertOneAdHoc collection session createWithId = insertOne collection session (createDocument createWithId)
+
+    let deleteOne (filter: _ Pred) (Collection collection) session =
+        match session with
+        | None -> collection.DeleteOne(filter)
+        | Some(Session session) -> collection.DeleteOne(session, filter)
+
+    let deleteMany (filter: _ Pred) (Collection collection) session =
+        match session with
+        | None -> collection.DeleteMany(filter)
+        | Some(Session session) -> collection.DeleteMany(session, filter)
+
+    let updateField (filter: _ Pred) (selector: Selector<_, _>) value ((Collection collection) as col) =
+        let update = Builders<_>.Update.Set(selector, value)
+        col, collection.UpdateOne(filter, update)
+
+    let updateFieldDefinition (selector: Selector<_, _>) value = Builders<_>.Update.Set(selector, value)
+
+    let updateCombine ((Collection collection) as col) (filter: _ Pred) (updateDefinitions: UpdateDefinition<_> seq) =
+        let update = Builders<_>.Update.Combine updateDefinitions
+        col, collection.UpdateOne(filter, update)
+
+    let replaceOne (filter: _ Pred) object ((Collection collection) as col) = col, collection.ReplaceOne(filter, object)
+
+    let replaceOneAsync (filter: _ Pred) object ((Collection collection) as col) =
+        async {
+            let! result = collection.ReplaceOneAsync(filter, object) |> Async.AwaitTask
+            return col, result }
+
+    let inline unwind (selector: Selector<_, _>) (query: _ IQueryable) = selector |> query.SelectMany
+
+    let inline nullToOption k =
+        if obj.ReferenceEquals(k, null) then None else Some k
---
+++ Serialization.fs
@@ -0,0 +1,125 @@
+module Flux.MongoDb.Serialization
+
+open MongoDB.Bson.Serialization.Serializers
+open Flux
+open MongoDB.Bson.Serialization
+open MongoDB.Bson
+open System
+open FSharp.Reflection
+
+type EmailSerializer() =
+    inherit SerializerBase<Email>()
+
+    override __.Serialize(context, _, value) =
+        value
+        |> Email.stringValue
+        |> context.Writer.WriteString
+
+    override this.Deserialize(context, _) =
+        let reader = context.Reader
+        match ()
+              |> reader.ReadString
+              |> Email.tryCreate with
+        | Ok email -> email
+        | Error _ -> raise (this.CreateCannotBeDeserializedException())
+
+    static member Register() = MongoDB.Bson.Serialization.BsonSerializer.RegisterSerializer<Email>(EmailSerializer())
+
+type NEStringSerializer() =
+    inherit SerializerBase<NEString>()
+
+    override __.Serialize(context, _, value) =
+        value
+        |> NEString.stringValue
+        |> context.Writer.WriteString
+
+    override this.Deserialize(context, _) =
+        let reader = context.Reader
+        match ()
+              |> reader.ReadString
+              |> NEString.tryCreate with
+        | Ok neStr -> neStr
+        | Error _ -> raise (this.CreateCannotBeDeserializedException())
+
+    static member Register() =
+        MongoDB.Bson.Serialization.BsonSerializer.RegisterSerializer<NEString>(NEStringSerializer())
+
+type ListSerializer<'T>() =
+    inherit SerializerBase<'T list>()
+
+    override __.Serialize(context, _, value) =
+        let writer = context.Writer
+        writer.WriteStartArray()
+        value |> List.iter (fun x -> BsonSerializer.Serialize(writer, typeof<'T>, x))
+        writer.WriteEndArray()
+
+    override __.Deserialize(context, _) =
+        let reader = context.Reader
+
+        match reader.CurrentBsonType with
+        | BsonType.Null ->
+            reader.ReadNull()
+            []
+        | BsonType.Array ->
+            seq {
+                reader.ReadStartArray()
+                while reader.ReadBsonType() <> BsonType.EndOfDocument do
+                    yield BsonSerializer.Deserialize<'T>(reader)
+                reader.ReadEndArray()
+            }
+            |> Seq.toList
+        | bsonType ->
+            sprintf "Can't deserialize a %s from BsonType %s" typeof<'T list>.FullName (bsonType.ToString())
+            |> InvalidOperationException
+            |> raise
+
+    interface IBsonArraySerializer with
+        member __.TryGetItemSerializationInfo serializationInfo =
+            let nominalType = typeof<'T>
+            serializationInfo <- BsonSerializationInfo(null, BsonSerializer.LookupSerializer<'T>(), nominalType)
+            true
+
+    static member Register() =
+        BsonSerializer.RegisterGenericSerializerDefinition(typeof<list<_>>, typeof<ListSerializer<_>>)
+
+module Conventions =
+
+    let isId str = str = "_id" || str.ToLower() = "id"
+
+    let shouldMapStringIdToObjectId (m: BsonMemberMap) =
+        m.MemberName.EndsWith "Id" && (m.MemberType.Equals typeof<string> || m.MemberType.Equals typeof<NEString>)
+
+let private memberName (property: Reflection.PropertyInfo) = property.Name
+
+let registerSerializers() =
+    ListSerializer<_>.Register()
+    EmailSerializer.Register()
+    NEStringSerializer.Register()
+
+let registerClassMapForRecord<'T> idAsObjectId isId shouldMapStringIdToObjectId additionalConfig =
+    printfn "Registering record %A" typeof<'T>
+    let ctor = FSharpValue.PreComputeRecordConstructorInfo(typeof<'T>)
+    let fields = FSharpType.GetRecordFields(typeof<'T>)
+    let fieldNames = fields |> Array.map (fun f -> f.Name)
+    BsonClassMap.RegisterClassMap<'T>
+        (Action<_>(fun cm ->
+            cm.MapConstructor(ctor, fieldNames) |> ignore
+            let (ids, properties) =
+                fields |> Array.partition (memberName >> (Option.defaultValue Conventions.isId isId))
+            ids
+            |> Array.head
+            |> cm.MapIdMember
+            |> ignore
+            properties
+            |> Array.iter
+                (cm.MapMember
+                 >> (fun m ->
+                     match Option.bind (fun mapper -> mapper m) shouldMapStringIdToObjectId with
+                     | Some true -> m.SetSerializer(StringSerializer(BsonType.ObjectId)) |> ignore
+                     | None when Conventions.shouldMapStringIdToObjectId m ->
+                         m.SetSerializer(StringSerializer(BsonType.ObjectId)) |> ignore
+                     | Some false -> ()
+                     | None -> ()))
+            do if idAsObjectId then cm.IdMemberMap.SetSerializer(StringSerializer(BsonType.ObjectId)) |> ignore
+            additionalConfig cm))
+    |> ignore
